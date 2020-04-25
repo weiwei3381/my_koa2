722755642e51386cb49f614252c92134424a8a1b
wowbat <weiwei3381@live.com>
2020-04-25 13:27:24 +0800

    3.7 代码重构, 增加初始化管理器, 使用绝对路径

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
 const Koa = require('koa')
-const Router = require('koa-router')
-const requireDirectory = require('require-directory')
+const InitManager = require('./core/init')
 
 const app = new Koa()
 
-// requireDirectory的第3个参数传入一个对象
-// 对象的visit属性可以传入一个回调函数,
-// 就是对每个模块进行导入成功后的操作
-// 需要确保路由模块都是采用default形式进行导出, 即module.exports = router
-requireDirectory(module, './api', {
-  visit: (obj) => {
-    // 如果是router对象,则在app上进行注册
-    if (obj instanceof Router) {
-      app.use(obj.routes())
-    }
-  },
-})
+// 将app传入到初始化类中
+InitManager.initCore(app)
 
 app.listen(3000)
 console.log('Server Start in port 3000!')
---
+++ core/init.js
@@ -0,0 +1,36 @@
+const requireDirectory = require('require-directory')
+const Router = require('koa-router')
+
+class InitManager {
+  /**
+   * 入口方法
+   * @param {Koa} app
+   */
+  static initCore(app) {
+    InitManager.app = app
+    // 使用静态方法需要用`类名.方法()`进行调用
+    InitManager.initLoadRouters()
+  }
+
+  /**
+   *  导入路由方法
+   */
+  static initLoadRouters() {
+    // 使用绝对路径引用api, 这样无论怎么修改init.js文件位置都没关系,
+    // 前提是api位置不能有变化
+    const apiDir = `${process.cwd()}/app/api`
+    requireDirectory(module, apiDir, {
+      visit: whenLoadModule,
+    })
+
+    // 当加载路由模块后的方法
+    function whenLoadModule(obj) {
+      // 如果是router对象,则在app上进行注册
+      if (obj instanceof Router) {
+        InitManager.app.use(obj.routes())
+      }
+    }
+  }
+}
+
+module.exports = InitManager
