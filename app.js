5b7ba02cf872af571e3aff5577aea39e91c5dbe0
wowbat <weiwei3381@live.com>
2020-04-25 16:57:00 +0800

    4.1 处理4种传参

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const Koa = require('koa')
 const InitManager = require('./core/init')
+const parser = require('koa-bodyparser')
 
 const app = new Koa()
+
+// 使用bodyParser, 以便解析body中的参数, 需要在初始化之前使用
+// 会自动把body的参数挂载到request对象的body属性中去
+app.use(parser())
 
 // 将app传入到初始化类中
 InitManager.initCore(app)
--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,9 +1,16 @@
 const Router = require('koa-router')
 
 const router = new Router() // 实例化router
-// 编写路由
-router.get('/api/v1/login', (ctx, next) => {
-  ctx.body = { key: '用户登录失败' }
+
+router.post('/v1/:id/test', (ctx, next) => {
+  const path = ctx.params // 获取到{id:"1"}
+  const query = ctx.request.query // 获取到{param:"weiwei"}
+  const headers = ctx.request.header // 对象包含很多属性, 其中token属性为12345678
+  // 在使用路由之前, 使用了koa-bodyparser中间件
+  // 才能在request的body属性中获取值
+  const body = ctx.request.body // 获取到{test: 2}
+
+  ctx.body = { key: '获取参数成功' }
 })
-// 由于需要批量自动导入, 这里必须采用默认导出形式
+
 module.exports = router
