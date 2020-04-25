4ec854b6876b17d32c37a3d90ef4f44a99c1e455
wowbat <weiwei3381@live.com>
2020-04-25 09:59:56 +0800

    为何引入路由

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 const Koa = require('koa')
 const app = new Koa()
 
-// 第1个中间件
 app.use(async (ctx, next) => {
-  await next()
-  // 在next之后,能保证后续代码已全部执行,才能拿到r
-  const r = ctx.r
-  console.log(r)
-})
-
-// 第2个中间件
-app.use(async (ctx, next) => {
-  const axios = require('axios')
-  const res = await axios.get('https://api.uomg.com/api/rand.qinghua')
-  // 将返回值挂载到ctx.r上
-  ctx.r = res
-  await next()
+  //查阅官网文档, 找到Request的路径和访问方式
+  // 为了简化操作, 官网文档说下面访问器和 Request 别名等效,所以可以直接在ctx上取
+  console.log(ctx.path)
+  console.log(ctx.method)
+  // 下面其实是完成路由功能
+  if (ctx.path === '/api/v1/login' && ctx.method === 'GET') {
+    // 返回值必须要放到ctx.body中才能显示, 不能直接return, 传入js对象可直接转换json
+    ctx.body = { key: '登录失败' }
+  }
 })
 
 // 监听3000端口
