f4ea68a8bf20610780081e0a96b983b7c2306965
wowbat <weiwei3381@live.com>
2020-04-26 19:19:37 +0800

    6.4 将加密改写到用户模型User中

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,4 +1,3 @@
-const bcrypt = require('bcryptjs') // 导入加密模块
 const Router = require('koa-router')
 const { RegisterValidator } = require('../../validators/validator')
 // 引入User模块
@@ -10,13 +9,9 @@ const router = new Router({
 
 router.post('/register', async (ctx) => {
   const v = await new RegisterValidator().validate(ctx)
-  // 传的参数表示生成盐的成本, 通常用10, 使用同步版本
-  const salt = bcrypt.genSaltSync(10)
-  // 两个用户密码即使一样, 加密后的密码也应该不一样, 以防止彩虹攻击
-  const psw = bcrypt.hashSync(v.get('body.password1'), salt)
   const user = {
     email: v.get('body.email'),
-    password: psw,
+    password: v.get('body.password1'),
     nickname: v.get('body.email'),
   }
   // 把用户保存至User表中, create是异步调用, 返回一个promise对象, 需要用await接收
--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs') // 导入加密模块
 const { Sequelize, Model } = require('sequelize')
 const { sequelize } = require('../../core/db')
 
@@ -16,7 +17,20 @@ User.init(
       type: Sequelize.STRING(128),
       unique: true,
     },
-    password: Sequelize.STRING,
+    password: {
+      type: Sequelize.STRING,
+      // set函数会在给password赋值的时候调用
+      // 这里相当于实现了一个观察者模式
+      set(val) {
+        // 传的参数表示生成盐的成本, 通常用10, 使用同步版本
+        const salt = bcrypt.genSaltSync(10)
+        // 两个用户密码即使一样, 加密后的密码也应该不一样, 以防止彩虹攻击
+        const psw = bcrypt.hashSync(val, salt)
+        // 将加密过后的密码存入数据库
+        // setDataValue是Model模型中的方法, 第一个参数表示给哪个参数赋值
+        this.setDataValue('password', psw)
+      },
+    },
     openid: {
       // 更加详细的设置每个属性的长度, 是否唯一等
       type: Sequelize.STRING(64),
