8944f933287248ed6cdac150cee601fcce2d755a
weiwei3381 <272885904@qq.com>
2020-04-26 10:02:02 +0800

    5.2 使用校验器获取参数

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,18 +1,16 @@
 const Router = require('koa-router')
-// 导入正整数校验器
 const { PositiveIntegerValidator } = require('../../validators/validator')
 
-const router = new Router() // 实例化router
+const router = new Router()
 
 router.post('/v1/:id/test', (ctx, next) => {
-  const path = ctx.params
-  const query = ctx.request.query
-  const headers = ctx.request.header
-  const body = ctx.request.body
-
-  // 实例化校验器后, 校验时需传入ctx参数
-  // 因为所有的参数都保存在ctx中,所以必须要传入ctx
   const v = new PositiveIntegerValidator().validate(ctx)
+  // 如果校验器通过了, 可以利用校验器获取参数,
+  // 分别用path,query,head,body代表路径,查询,head和body中的参数
+  // 例如path.id代表获取路径中的id参数
+  // validator会自动进行转型, 如果不需要转型则第2个参数传false
+  // 也能获取嵌套层级, 例如'body.a.b'
+  const id = v.get('path.id', false)
 
   ctx.body = { key: '获取参数成功' }
 })
