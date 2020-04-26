5994487ae33531620d2cf6344d79ac7a13620146
wowbat <weiwei3381@live.com>
2020-04-26 19:07:54 +0800

    6.3 用户密码加盐加密

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs') // 导入加密模块
 const Router = require('koa-router')
 const { RegisterValidator } = require('../../validators/validator')
 // 引入User模块
@@ -8,13 +9,14 @@ const router = new Router({
 })
 
 router.post('/register', async (ctx) => {
-  // 新版本validator都需要加上await
-  // validate需要放到代码的第一行, 否则起不到守门的作用
   const v = await new RegisterValidator().validate(ctx)
-  // 获取用户参数信息, 由于已经验证过了, 所以直接可以用
+  // 传的参数表示生成盐的成本, 通常用10, 使用同步版本
+  const salt = bcrypt.genSaltSync(10)
+  // 两个用户密码即使一样, 加密后的密码也应该不一样, 以防止彩虹攻击
+  const psw = bcrypt.hashSync(v.get('body.password1'), salt)
   const user = {
     email: v.get('body.email'),
-    password: v.get('body.password1'),
+    password: psw,
     nickname: v.get('body.email'),
   }
   // 把用户保存至User表中, create是异步调用, 返回一个promise对象, 需要用await接收
