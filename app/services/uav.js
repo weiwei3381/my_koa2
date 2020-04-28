969541fc23885324c6182929384285f0fe52afc7
weiwei3381 <272885904@qq.com>
2020-04-28 11:20:31 +0800

    util类

--- app/services/uav.js
+++ app/services/uav.js
@@ -1,1 +1,9 @@
-class UAVManager {}
+const util = require('util')
+
+// 无人机管理类
+class UAVManager {
+  static async codeToToken(code) {
+    const id = 'weiwei'
+    const url = util.format('测试%s', id)
+  }
+}
